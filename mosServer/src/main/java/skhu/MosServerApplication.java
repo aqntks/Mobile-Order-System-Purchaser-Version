bfd3b51bafa835bd0ee02b272b07dc3d41ef1fe0
aqntks <42826598+aqntks@users.noreply.github.com>
2020-04-13 19:07:08 +0900

    서버 실험용

---
+++ mosServer/src/main/java/skhu/MosServerApplication.java
@@ -0,0 +1,13 @@
+package skhu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosServerApplication.class, args);
+	}
+
+}
---
+++ mosServer/src/test/java/skhu/MosServerApplicationTests.java
@@ -0,0 +1,13 @@
+package skhu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MosServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
